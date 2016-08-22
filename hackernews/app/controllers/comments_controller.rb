1c0a3625f00e2494e21ffabf5ed4b55230ccf25f
Alex S & Kit Robinson <pair+alexstevenson19.kitrobinson@devbootcamp.com>
2016-08-22 16:19:53 -0500

    add create, delete, read comments.  Update seems unnecessary.  Still using placeholder user

--- hackernews/app/controllers/comments_controller.rb
+++ hackernews/app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
   def index
+    @article = Article.find(params[:article_id])
   end
 
   def new
   end
 
   def create
+    p params
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+    @comment.commenter_id = 1
+
+    @comment.save
+    redirect_to [Article.find(params[:article_id]), :comments]
   end
 
   def show
@@ -18,5 +26,13 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to [Article.find(params[:article_id]), :comments]
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
