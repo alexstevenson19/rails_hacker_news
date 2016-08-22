2056c46baa7dece9d23fe408ed686471f251eadb
Alex S & Kit Robinson <pair+alexstevenson19.kitrobinson@devbootcamp.com>
2016-08-22 15:18:06 -0500

    Article create, edit and destroying paths and views built

--- hackernews/app/controllers/articles_controller.rb
+++ hackernews/app/controllers/articles_controller.rb
@@ -22,12 +22,23 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to articles_path
   end
 
 private
